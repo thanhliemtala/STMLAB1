637d5926afcf03a303facf41a991f2577e9f6e0c
DESKTOP-SEAP631\84334 <liem.nguyen2711@hcmut.edu.vn>
2023-09-20 16:36:32 +0700

    init for Ex1

---
+++ SrcFile/Core/Inc/global.h
@@ -0,0 +1,21 @@
+/*
+ * global.h
+ *
+ *  Created on: Sep 20, 2023
+ *      Author: 84334
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "software_timer.h"
+//#include "button.h"
+#include "main.h"
+#define INIT		1
+#define	AUTO_RED	2
+#define AUTO_YELLOW	3
+
+extern int status;
+
+
+#endif /* INC_GLOBAL_H_ */
---
+++ SrcFile/Core/Src/global.c
@@ -0,0 +1,10 @@
+/*
+ * global.c
+ *
+ *  Created on: Sep 20, 2023
+ *      Author: 84334
+ */
+
+#include "global.h"
+int status = 0;
+
