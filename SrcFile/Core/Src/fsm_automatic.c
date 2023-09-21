d2afc2646a392b079f0c48462ab24d752a567ff7
DESKTOP-SEAP631\84334 <liem.nguyen2711@hcmut.edu.vn>
2023-09-21 08:08:47 +0700

    UPDATE FOR EX2

--- SrcFile/Core/Src/fsm_automatic.c
+++ SrcFile/Core/Src/fsm_automatic.c
@@ -11,14 +11,14 @@ void fsm_automatic_run(){
 	case INIT:
 		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
 		status = AUTO_RED;
 		setTimer1(500);
 		break;
 	case AUTO_RED:
 		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
 		if(timer1_flag == 1){
 			status = AUTO_YELLOW;
 			setTimer1(500);
@@ -27,7 +27,7 @@ void fsm_automatic_run(){
 	case AUTO_YELLOW:
 		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
 		if(timer1_flag == 1){
 			status = AUTO_GREEN;
 			setTimer1(200);
@@ -36,7 +36,7 @@ void fsm_automatic_run(){
 	case AUTO_GREEN:
 		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
 		if(timer1_flag == 1){
 			status = AUTO_RED;
 			setTimer1(300);
