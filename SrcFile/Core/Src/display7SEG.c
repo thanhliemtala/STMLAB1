fb7361d9c9d9aaed01700cd2d2c812a33adf9200
DESKTOP-SEAP631\84334 <liem.nguyen2711@hcmut.edu.vn>
2023-10-02 10:14:50 +0700

    Ex4 last update

--- SrcFile/Core/Src/display7SEG.c
+++ SrcFile/Core/Src/display7SEG.c
@@ -76,7 +76,7 @@ void display7SEG(int num){
 		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(b_GPIO_Port,b_Pin, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(c_GPIO_Port,c_Pin, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(d_GPIO_Port,d_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(d_GPIO_Port,d_Pin, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(e_GPIO_Port,e_Pin, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(f_GPIO_Port,f_Pin, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(g_GPIO_Port,g_Pin, GPIO_PIN_SET);
